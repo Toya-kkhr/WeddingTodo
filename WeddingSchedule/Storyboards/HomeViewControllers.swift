cfeb385606e8d32fb902f19c8b61e50677a58824
Toya Kakohara <toyack1128@gmail.com>
2020-10-30 12:30:58 +0900

    first commit

---
+++ WeddingSchedule/Storyboards/HomeViewControllers.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewControllers.swift
+//  WeddingSchedule
+//
+//  Created by 加古原　冬弥 on 2020/10/28.
+//  Copyright © 2020 加古原　冬弥. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewControllers: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ WeddingSchedule/Storyboards/TodoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TodoViewController.swift
+//  WeddingSchedule
+//
+//  Created by 加古原　冬弥 on 2020/10/28.
+//  Copyright © 2020 加古原　冬弥. All rights reserved.
+//
+
+import UIKit
+
+class TodoViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
